d1125c1fcf4caf67a832cd316c442aa93ae743d2
PtrZjc <ptr.zjc@gmail.com>
2019-02-20 21:42:31 +0100

    finished form with validation

--- js/app.js
+++ js/app.js
@@ -1,12 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     var top = document.querySelector("#top")
+    var addBtn = document.querySelector("#add-btn")
 
-    function fillInfo(parent){
-        console.log(parent.target.blah)
-        console.log()
+    loadBooks()
+
+
+
+    function loadBooks() {
         $.ajax({
-            url: "http://localhost:8282/books/"+parent.dataset.id,
+            url: "http://localhost:8282/books/",
+            data: {},
+            type: "GET",
+            dataType: "json"
+        }).done(function (result) {
+
+            result.forEach(function (e) {
+                var newBook = document.createElement("ul");
+                top.appendChild(newBook)
+                newBook.classList.add("bookTitle")
+                newBook.dataset.id = e.id
+                newBook.innerHTML = "<h3>" + e.title + "</h3>"
+            });
+
+            var newBookEls = document.querySelectorAll('.bookTitle')
+
+            newBookEls.forEach(function (e) {
+
+                e.addEventListener('click', function e() {
+                    fillInfo(this)
+                    this.removeEventListener('click', e)
+                })
+            })
+        }).fail(function (xhr, status, err) {
+        }).always(function (xhr, status) {
+        })
+    }
+
+    function fillInfo(parent) {
+        $.ajax({
+            url: "http://localhost:8282/books/" + parent.dataset.id,
             data: {},
             type: "GET",
             dataType: "json"
@@ -16,45 +49,111 @@ document.addEventListener("DOMContentLoaded", function () {
             data.push(document.createElement('li'))
             data.push(document.createElement('li'))
             data.push(document.createElement('li'))
-            data.push(document.createElement('li')) 
-          for (const information of data) {
-              parent.appendChild(information)
-          }
+            data.push(document.createElement('li'))
+            for (const information of data) {
+                parent.appendChild(information)
+            }
             data[0].innerText = "Isbn: " + result.isbn
             data[1].innerText = "Author: " + result.author
             data[2].innerText = "Publisher: " + result.publisher
             data[3].innerText = "Type: " + result.type
+
         }).fail(function (xhr, status, err) {
-        }).always(function (xhr, status) {   
+        }).always(function (xhr, status) {
         });
-    }  
+    }
+
+    addBtn.disable = true
+    addBtn.addEventListener('click', function () {
+        var title = document.querySelector("#title")
+        var author = document.querySelector("#author")
+        var type = document.querySelector("#type")
+        var publisher = document.querySelector("#publisher")
+        var isbn = document.querySelector("#isbn")
+        var flag = true //tak bedziemy sprawdzac poprawność danych
         
-    $.ajax({
-        url: "http://localhost:8282/books/",
-        data: {},
-        type: "GET",
-        dataType: "json"
-    }).done(function (result) {
+        if (title.value.length === 0) {
+            addAlert(title)
+        }else{
+            removeAlert(title)
+        }
+        if (author.value.length === 0) {
+            addAlert(author)
+        }else{
+            removeAlert(author)
+        }
+        if (type.value.length === 0) {
+            addAlert(type)
+        }else{
+            removeAlert(type)
+        }
+        if (publisher.value.length === 0) {
+            addAlert(publisher)
+        }else{
+            removeAlert(publisher)
+        }
+        if (isbn.value.length !== 10 || isNaN(isbn.value)) {
+            addAlert(isbn)
+        }else{
+            removeAlert(isbn)
+        }
 
-        result.forEach(function (e) {
-            var newBook = document.createElement("ul");
-            top.appendChild(newBook)
-            newBook.classList.add("bookTitle")
-            newBook.dataset.id = e.id
-            newBook.innerHTML = "<h1>"+e.title+"</h1>"
-        });
+        //TODO zastanowić się, jak zaktywować przycisk
+        function addAlert(input) {
+            if (input.parentElement.querySelector(".alert") !== null) {
+                return
+            }
 
-        var newBookEls = document.querySelectorAll('.bookTitle')
+            document.querySelector('#author')
+            var missingInput = document.createElement("div")
+            missingInput.classList.add('alert','alert-danger')
+            input.parentElement.insertBefore(missingInput, input)
+            input.classList.add("border-danger")
+            if (input !== isbn) {
+                missingInput.innerText = "Missing input"
+            } else {
+                missingInput.innerText = "Wrong ISBN. It should consist of 10 digits."
+            }
+        }
 
-        newBookEls.forEach(function(e){
+        function removeAlert(input) {
+            if (input.parentElement.querySelector(".alert") === null) {
+                return
+            }
+            var toDelete = input.parentElement.querySelector('.alert')
+            toDelete.parentElement.removeChild(toDelete);
+            input.classList.remove('border-danger')
+        }
 
-            $(e).one('click',function(event){
-                fillInfo(this)
-            })
 
 
-    }).fail(function (xhr, status, err) {
-    }).always(function (xhr, status) {
-    });
+        //     
+        //     var wrongISBN
+
+
+        //     <div class="">
+        //     Missing input
+        //   </div>
+
+        var book = {
+            "isbn": isbn,
+            "title": title,
+            "author": author,
+            "publisher": publisher,
+            "type": type
+        }
+
+        // $.ajax({
+        //     url: "http://localhost:8282/books/" + parent.dataset.id,
+        //     data: ,
+        //     type: "POST",
+        //     dataType: "json"
+        // }).done(function (result) {
+        // }).fail(function (xhr, status, err) {
+        // }).always(function (xhr, status) {
+        // });
+
+
+    })
 
 })

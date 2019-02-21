55583b7049bfdfe1599c688e8d8885697e1cac9f
PtrZjc <ptr.zjc@gmail.com>
2019-02-21 22:10:54 +0100

    finished adding books

--- js/app.js
+++ js/app.js
@@ -7,20 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    function loadBooks() {
+    function loadBooks(id) {
         $.ajax({
             url: "http://localhost:8282/books/",
             data: {},
             type: "GET",
             dataType: "json"
         }).done(function (result) {
-
             result.forEach(function (e) {
-                var newBook = document.createElement("ul");
-                top.appendChild(newBook)
-                newBook.classList.add("bookTitle")
-                newBook.dataset.id = e.id
-                newBook.innerHTML = "<h3>" + e.title + "</h3>"
+                appendBookEl(e.id, e.title)
             });
 
             var newBookEls = document.querySelectorAll('.bookTitle')
@@ -35,6 +30,117 @@ document.addEventListener("DOMContentLoaded", function () {
         }).fail(function (xhr, status, err) {
         }).always(function (xhr, status) {
         })
+    }
+
+    addBtn.addEventListener('click', function () {
+        var title = document.querySelector("#title")
+        var author = document.querySelector("#author")
+        var type = document.querySelector("#type")
+        var publisher = document.querySelector("#publisher")
+        var isbn = document.querySelector("#isbn")
+        var flag = true //tak bedziemy sprawdzac poprawność danych
+
+        if (title.value.length === 0) {
+            addAlert(title)
+            flag = false
+        } else {
+            removeAlert(title)
+        }
+        if (author.value.length === 0) {
+            addAlert(author)
+            flag = false
+        } else {
+            removeAlert(author)
+        }
+        if (type.value.length === 0) {
+            addAlert(type)
+            flag = false
+        } else {
+            removeAlert(type)
+        }
+        if (publisher.value.length === 0) {
+            addAlert(publisher)
+            flag = false
+        } else {
+            removeAlert(publisher)
+        }
+        if (isbn.value.length !== 10 || isNaN(isbn.value)) {
+            addAlert(isbn)
+            flag = false
+        } else {
+            removeAlert(isbn)
+        }
+
+        if (flag) {
+            document.querySelectorAll("input").forEach(function (el) {
+                el.value = ""
+            })
+
+            var dock = document.querySelector('.clearfix')
+            var successAlert = document.createElement("div")
+            successAlert.classList.add('alert', 'alert-success', 'alert-dismissible')
+            dock.parentElement.insertBefore(successAlert, dock)
+            successAlert.innerHTML = '<button type="button" class="close" data-dismiss="alert">&times;</button>            <strong>Success!</strong> Book added to the collection.'
+
+            var book = {
+                "isbn": isbn.value,
+                "title": title.value,
+                "author": author.valuer,
+                "publisher": publisher.value,
+                "type": type.value
+            }
+
+            $.ajax({
+                url: "http://localhost:8282/books/",
+                data: JSON.stringify(book),
+                type: "POST",
+                dataType: "json",
+                contentType: "application/json"
+            }).done(function (result) {
+                appendBookEl(result.id, result.title)
+                var newBook = document.querySelector('[data-id="'+result.id+'"]')
+                newBook.addEventListener('click', function e() {
+                    fillInfo(this)
+                    this.removeEventListener('click', e)
+                })
+            }).fail(function (xhr, status, err) {
+            }).always(function (xhr, status) {
+            });
+        }
+
+        function addAlert(input) {
+            if (input.parentElement.querySelector(".alert") !== null) {
+                return
+            }
+
+            document.querySelector('#author')
+            var missingInput = document.createElement("div")
+            missingInput.classList.add('alert', 'alert-danger')
+            input.parentElement.insertBefore(missingInput, input)
+            input.classList.add("border-danger")
+            if (input !== isbn) {
+                missingInput.innerText = "Missing input"
+            } else {
+                missingInput.innerText = "Wrong ISBN. It should consist of 10 digits."
+            }
+        }
+
+        function removeAlert(input) {
+            if (input.parentElement.querySelector(".alert") === null) {
+                return
+            }
+            var toDelete = input.parentElement.querySelector('.alert')
+            toDelete.parentElement.removeChild(toDelete);
+            input.classList.remove('border-danger')
+        }
+    })
+
+    function appendBookEl(id, title) {
+        var newBook = document.createElement("ul");
+        top.appendChild(newBook)
+        newBook.classList.add("bookTitle")
+        newBook.dataset.id = id
+        newBook.innerHTML = "<h3>" + title + "</h3>"
     }
 
     function fillInfo(parent) {
@@ -63,97 +169,5 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    addBtn.disable = true
-    addBtn.addEventListener('click', function () {
-        var title = document.querySelector("#title")
-        var author = document.querySelector("#author")
-        var type = document.querySelector("#type")
-        var publisher = document.querySelector("#publisher")
-        var isbn = document.querySelector("#isbn")
-        var flag = true //tak bedziemy sprawdzac poprawność danych
-        
-        if (title.value.length === 0) {
-            addAlert(title)
-        }else{
-            removeAlert(title)
-        }
-        if (author.value.length === 0) {
-            addAlert(author)
-        }else{
-            removeAlert(author)
-        }
-        if (type.value.length === 0) {
-            addAlert(type)
-        }else{
-            removeAlert(type)
-        }
-        if (publisher.value.length === 0) {
-            addAlert(publisher)
-        }else{
-            removeAlert(publisher)
-        }
-        if (isbn.value.length !== 10 || isNaN(isbn.value)) {
-            addAlert(isbn)
-        }else{
-            removeAlert(isbn)
-        }
-
-        //TODO zastanowić się, jak zaktywować przycisk
-        function addAlert(input) {
-            if (input.parentElement.querySelector(".alert") !== null) {
-                return
-            }
-
-            document.querySelector('#author')
-            var missingInput = document.createElement("div")
-            missingInput.classList.add('alert','alert-danger')
-            input.parentElement.insertBefore(missingInput, input)
-            input.classList.add("border-danger")
-            if (input !== isbn) {
-                missingInput.innerText = "Missing input"
-            } else {
-                missingInput.innerText = "Wrong ISBN. It should consist of 10 digits."
-            }
-        }
-
-        function removeAlert(input) {
-            if (input.parentElement.querySelector(".alert") === null) {
-                return
-            }
-            var toDelete = input.parentElement.querySelector('.alert')
-            toDelete.parentElement.removeChild(toDelete);
-            input.classList.remove('border-danger')
-        }
-
-
-
-        //     
-        //     var wrongISBN
-
-
-        //     <div class="">
-        //     Missing input
-        //   </div>
-
-        var book = {
-            "isbn": isbn,
-            "title": title,
-            "author": author,
-            "publisher": publisher,
-            "type": type
-        }
-
-        // $.ajax({
-        //     url: "http://localhost:8282/books/" + parent.dataset.id,
-        //     data: ,
-        //     type: "POST",
-        //     dataType: "json"
-        // }).done(function (result) {
-        // }).fail(function (xhr, status, err) {
-        // }).always(function (xhr, status) {
-        // });
-
-
-    })
 
 })
